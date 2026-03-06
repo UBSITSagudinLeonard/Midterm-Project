2b58ff8de162a280c6d6168dde979dd66f680018
Leonard Sagudin <20244652@s.ubaguio.edu>
2026-03-06 10:40:44 +0800

    Initial commit

---
+++ src/app/app.routes.ts
@@ -0,0 +1,21 @@
+import { Routes } from '@angular/router';
+import { Home } from './comps/home/home';
+import { Navi } from './comps/navi/navi';
+import { Attribute } from './databinding/attribute/attribute';
+import { Class } from './databinding/class/class';
+import { EventBinding } from './databinding/event/event';
+import { Interpolation } from './databinding/interpolation/interpolation';
+import { Property } from './databinding/property/property';
+import { Style } from './databinding/style/style';
+import { TwoWayBinding } from './databinding/two-way/two-way';
+
+
+export const routes: Routes = [{path: "home",
+component: Home}, {path: "navi", component: Navi},
+{path: "attribute", component: Attribute},
+{path: "class", component: Class}, 
+{path: "event", component: EventBinding}, 
+{path: "interpolation", component: Interpolation}, 
+{path: "property", component: Property},
+{path: "style", component: Style},
+{path: "two-way", component: TwoWayBinding}];
